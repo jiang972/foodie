f90529b3798e0a3005166af41d3ee36da0a0cfc0
jiangyuxin <972166944@qq.com>
2020-10-06 16:45:15 +0800

    init

---
+++ foodie-api/src/main/java/com/jyx/controller/hello.java
@@ -0,0 +1,13 @@
+package com.jyx.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "helloWorld";
+    }
+}
