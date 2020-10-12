14a04491a763616bf5399cf488e2a4a58bc67d3d
jiangyuxin <972166944@qq.com>
2020-10-12 19:37:04 +0800

    商品信息

---
+++ foodie-service/src/main/java/com/jyx/service/ItemService.java
@@ -0,0 +1,40 @@
+package com.jyx.service;
+
+import com.jyx.pojo.Items;
+import com.jyx.pojo.ItemsImg;
+import com.jyx.pojo.ItemsParam;
+import com.jyx.pojo.ItemsSpec;
+
+import java.util.List;
+
+public interface ItemService {
+
+    /**
+     * 根据商品ID查询详情
+     * @param itemId
+     * @return
+     */
+    public Items queryItemById(String itemId);
+
+    /**
+     * 根据商品id查询商品图片列表
+     * @param itemId
+     * @return
+     */
+    public List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品id查询商品规格
+     * @param itemId
+     * @return
+     */
+    public List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品id查询商品参数
+     * @param itemId
+     * @return
+     */
+    public ItemsParam queryItemParam(String itemId);
+
+}
