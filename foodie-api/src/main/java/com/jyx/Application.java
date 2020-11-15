e5a8cab072752bc0248a2af24e2337b16eb929e2
jiangyuxin <972166944@qq.com>
2020-11-15 22:05:24 +0800

    关闭订单

--- foodie-api/src/main/java/com/jyx/Application.java
+++ foodie-api/src/main/java/com/jyx/Application.java
@@ -3,11 +3,13 @@ package com.jyx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.jyx.mapper")
 @ComponentScan(basePackages = {"com.jyx","org.n3r.idworker"})
+@EnableScheduling //开启定时
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
